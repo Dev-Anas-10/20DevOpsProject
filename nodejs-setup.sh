aeee2e6607890c90a02c3d24c0ea85f7e4c415a1
Anas <anasashraf1170@gmail.com>
2024-11-30 17:27:05 +0200

    last update

---
+++ nodejs-setup.sh
@@ -0,0 +1,5 @@
+https://nodejs.org/en/download/package-manager
+
+sudo chown -R $USER:$USER /root/dir/xxxxxxx
+
+git clone -b 4.Containerization-of-emartapp-using-docker https://github.com/Dev-Anas-10/20DevOpsProject.git
